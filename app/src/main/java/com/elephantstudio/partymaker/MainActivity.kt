d5597441bab52caa9b4be032198a565e4cc77c99
limona <mateusz.walaszek@op.pl>
2023-02-05 16:13:01 +0100

    add collapsing toolbar layout

--- app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
+++ app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.elephantstudio.partymaker
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.elephantstudio.partymaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
