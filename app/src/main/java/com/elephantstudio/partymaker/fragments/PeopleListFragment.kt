f63f66637939bddc7e49652cbec8da4d7c1f3cd2
limona <mateusz.walaszek@op.pl>
2023-02-06 14:58:09 +0100

    basic layout add recyclerView add people counter

---
+++ app/src/main/java/com/elephantstudio/partymaker/fragments/PeopleListFragment.kt
@@ -0,0 +1,105 @@
+package com.elephantstudio.partymaker.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.elephantstudio.partymaker.adapters.PeopleListAdapter
+import com.elephantstudio.partymaker.data.Person
+import com.elephantstudio.partymaker.databinding.FragmentPeopleListBinding
+
+class PeopleListFragment : Fragment() {
+
+    private var _binding: FragmentPeopleListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var peopleListAdapter: PeopleListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentPeopleListBinding.inflate(inflater, container, false)
+
+        setupRecyclerView()
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupRecyclerView() {
+
+        val peopleList = listOf(
+            Person(
+                "Dawid Wikło",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+            Person(
+                "Dominik Kasper",
+                null
+            ),
+            Person(
+                "Marcin Musiała",
+                null
+            ),
+        )
+
+        binding.rvPeopleList.apply {
+            peopleListAdapter = PeopleListAdapter(peopleList)
+            adapter = peopleListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+}
