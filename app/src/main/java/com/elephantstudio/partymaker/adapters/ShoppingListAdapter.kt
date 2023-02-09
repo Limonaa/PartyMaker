4dce4467d0e72b883c4bb89c5a94941073fe61a3
limona <mateusz.walaszek@op.pl>
2023-02-09 17:45:36 +0100

    add shopping list fragment add data class for shopping articles add shopping list adapter

---
+++ app/src/main/java/com/elephantstudio/partymaker/adapters/ShoppingListAdapter.kt
@@ -0,0 +1,32 @@
+package com.elephantstudio.partymaker.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.elephantstudio.partymaker.data.Article
+import com.elephantstudio.partymaker.databinding.ItemShopArticleBinding
+
+class ShoppingListAdapter(
+    private var shoppingList: List<Article>
+): RecyclerView.Adapter<ShoppingListAdapter.ShoppingListViewHolder>() {
+
+    inner class ShoppingListViewHolder(val binding: ItemShopArticleBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingListViewHolder {
+
+        return ShoppingListViewHolder(ItemShopArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ShoppingListViewHolder, position: Int) {
+
+        holder.binding.apply {
+            tvArticleName.text = shoppingList[position].articleName
+            tvArticleBuyer.text = shoppingList[position].articleBuyer
+            tvArticlePrice.text = shoppingList[position].articlePrice.toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shoppingList.size
+    }
+}
---
+++ app/src/main/java/com/elephantstudio/partymaker/data/Article.kt
@@ -0,0 +1,7 @@
+package com.elephantstudio.partymaker.data
+
+data class Article(
+    val articleName: String,
+    val articleBuyer: String,
+    val articlePrice: Int,
+)
