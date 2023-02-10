19af32feaa8dcf617b98e40e479432b3a993c8e4
czaro <cezarykalinowski05@gmail.com>
2023-02-10 18:47:25 +0100

    add bottom navigation bar

--- app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
+++ app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
@@ -3,8 +3,15 @@ package com.elephantstudio.partymaker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.elephantstudio.partymaker.databinding.ActivityMainBinding
+import com.elephantstudio.partymaker.fragments.PartyFragment
+import com.elephantstudio.partymaker.fragments.PeopleListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +23,27 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
 
+        val partyFragment = PartyFragment()
+        val peopleListFragment = PeopleListFragment()
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.partyFragment -> { setFragment(partyFragment)
+
+                }
+                R.id.peopleListFragment2 -> {setFragment(peopleListFragment)
+
+                }
+            }
+            true
+        }
+
+
+
 
 //        setSupportActionBar(binding.toolbar)
 //
@@ -31,6 +57,16 @@ class MainActivity : AppCompatActivity() {
 //                .setAction("Action", null).show()
 //        }
     }
+
+    private fun setFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.bottomNavFrame, fragment)
+            commit()
+        }
+    }
+
+
 //
 //    override fun onCreateOptionsMenu(menu: Menu): Boolean {
 //
