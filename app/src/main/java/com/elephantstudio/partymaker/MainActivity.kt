0f2ebf0b33c80df5b032e4310937166340257879
czaro <cezarykalinowski05@gmail.com>
2023-02-10 19:51:53 +0100

    add and fix bottom navigation bar

--- app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
+++ app/src/main/java/com/elephantstudio/partymaker/MainActivity.kt
@@ -3,15 +3,8 @@ package com.elephantstudio.partymaker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.elephantstudio.partymaker.databinding.ActivityMainBinding
-import com.elephantstudio.partymaker.fragments.PartyFragment
-import com.elephantstudio.partymaker.fragments.PeopleListFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,49 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-
-        val partyFragment = PartyFragment()
-        val peopleListFragment = PeopleListFragment()
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.partyFragment -> { setFragment(partyFragment)
-
-                }
-                R.id.peopleListFragment2 -> {setFragment(peopleListFragment)
-
-                }
-            }
-            true
-        }
-
-
-
-
-//        setSupportActionBar(binding.toolbar)
-//
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAnchorView(R.id.fab)
-//                .setAction("Action", null).show()
-//        }
     }
 
-    private fun setFragment(fragment: Fragment) {
-
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.bottomNavFrame, fragment)
-            commit()
-        }
-    }
 
 
 //
